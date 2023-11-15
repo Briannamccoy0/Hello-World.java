522b41a7a245e3d1d57fc18ae08a93471c21f7cc
briannamccoy0 <briannamccoy84@gmail.com>
2023-11-15 17:58:39 -0500

    the thing

---
+++ src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Semicolon here!"); //  /* Comment TIIIME! */
+        System.out.println("Next to me too!");
+        System.out.println("Ellllllooooo Peoplesssss");
+
+            // This is a single line comment
+            
+            /* This is a
+            
+            multi-line
+            
+            comment */
+
+                
+            String text = "Hello!"; // New variable
+                
+            text = "Citrus"; // Ok!
+                
+            text = "5.0"; // Ok!
+                
+            text = "7.2"; // Error!
+
+
+                // "Citrus Circuits"
+                
+                String variable = "Citrus " + "Circuits";
+
+
+             
+
+    }
+}
